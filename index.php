40b82b2cf92998cce6c16fbdc0c152afac30dd2b
kowru21 <kovalev-pavel2002@mail.ru>
2024-09-23 17:20:12 +0500

    sql connect

--- index.php
+++ index.php
@@ -5,6 +5,33 @@
                         Установите гугл хром, ваш браузер плохой
                     </audio>
 
+<?php
+
+    //Connect to the database
+    $host = "sql155120.byetcluster.com";   //See Step 3 about how to get host name
+    $user = "if0_37368287";                     //Your Cloud 9 username
+    $pass = "wanzqICkNP";                                 //Remember, there is NO password!
+    $db = "if0_37368287_db1";                          //Your database name you want to connect to
+    $port = 3306;                               //The port #. It is always 3306
+
+    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+
+
+
+    //And now to perform a simple query to make sure it's working
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
+    }
+
+?>
+
+
+
+
+
 <div class="video-bg">
    <video src="vecteezy_colorful-candy-rainbow-bright-blurry-gradient-abstract_28301464.mp4" type="video/mp4" playsinline autoplay muted loop></video>
    <div class="effects"></div>
